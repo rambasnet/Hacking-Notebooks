dcf4aebfd4f6592f76b3abb65e085f4fdf500f94
Ram Basnet <rambasnet@gmail.com>
2019-08-08 13:23:56 -0600

    updated FormatStrings

--- booksrc/fmt_vuln.c
+++ booksrc/fmt_vuln.c
@@ -15,14 +15,13 @@ int main(int argc, char *argv[]) {
    printf("The right way to print user-controlled input:\n");
    printf("%s", text);
 
-
    printf("\nThe wrong way to print user-controlled input:\n");
    printf(text);
 
    printf("\n");
 
    // Debug output
-   printf("[*] test_val @ 0x%08x = %d 0x%08x\n", &test_val, test_val, test_val);
+   printf("[*] test_val @ %08x = %d %08x\n", &test_val, test_val, test_val);
 
    exit(0);
 }
