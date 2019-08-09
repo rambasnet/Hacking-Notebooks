0173a167b0bd4697ef016e0d21478f6e76f00925
Ram Basnet <rambasnet@gmail.com>
2019-08-08 21:25:04 -0600

    updated FormatStrings

---
+++ booksrc/fmt_strings.c
@@ -0,0 +1,21 @@
+// fmt_strings.c
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   char string[10];
+   int A = -73;
+   unsigned int B = 31337;
+
+   strcpy(string, "sample");
+
+   // Example of printing with different format string
+   printf("[A] Dec: %d, Hex: %x, Unsigned: %u\n", A, A, A);
+   printf("[B] Dec: %d, Hex: %x, Unsigned: %u\n", B, B, B);
+   printf("[field width on B] 3: '%3u', 10: '%10u', '%08u'\n", B, B, B);
+   printf("[string] %s  Address %p\n", string, string);
+
+   // Example of unary address operator (dereferencing) and a %x format string
+   printf("variable A is at address: %p\n", &A);
+}
--- booksrc/fmt_vuln.c
+++ booksrc/fmt_vuln.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
    printf("\n");
 
    // Debug output
-   printf("[*] test_val @ %08x = %d %08x\n", &test_val, test_val, test_val);
+   printf("[*] test_val @ 0x%08x = %d 0x%08x\n", &test_val, test_val, test_val);
 
    exit(0);
 }
