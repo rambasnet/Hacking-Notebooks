e1bd3d20c2ce38fdb8f5323574b211b0a0ab85cf
Ram Basnet <rambasnet@gmail.com>
2019-09-05 00:05:15 -0600

    added fmt_vuln2.c

---
+++ fmt_vuln2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void badfunction(char *data) {
+    char input[200];
+    printf("Enter your name: ");
+    // scanf gives segfault
+    //scanf("%s", input);
+    // fgets doesn't work!
+    //fgets(input, sizeof(input)-1, stdin);
+    strcpy(input, data);
+    printf("Hello, ");
+    printf(input);
+    printf("\ninput is @ 0x%08x\n", &input);
+    register int ebp asm("ebp");
+    printf("return address @ 0x%08x\n", (ebp+4));
+    // would be be nice to be able to print value at return add
+}
+
+int main(int argc, char* argv[]) {
+    badfunction(argv[1]);
+    printf("%s\n", "good bye!");
+    return 0;
+}
